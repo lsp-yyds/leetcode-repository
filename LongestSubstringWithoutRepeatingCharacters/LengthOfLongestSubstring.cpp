d44544716704210a8df525a8d2d78f845f43815b
lsp-yyds <82454014+lsp-yyds@users.noreply.github.com>
2023-02-01 14:19:35 +0800

    Finish LongestSubstring

---
+++ LongestSubstringWithoutRepeatingCharacters/LengthOfLongestSubstring.cpp
@@ -0,0 +1,45 @@
+//
+// Created by bruce on 2/1/23.
+//
+
+#include <iostream>
+#include <unordered_set>
+
+bool checkRepetition(std::string& s, int start, int end) {
+    std::unordered_set<char> chars;
+
+    for (int i = start; i <= end; ++i) {
+        char c = s[i];
+
+        if (chars.count(c)) {
+            return false;
+        }
+        chars.insert(c);
+    }
+
+    return true;
+}
+
+class Solution {
+  public:
+    static int lengthOfLongestSubstring(std::string s) {
+
+        int res{0};
+        for (int i = 0; i < s.size(); ++i) {
+            for (int j = i; j < s.size(); ++j) {
+                if (checkRepetition(s, i, j)) {
+                    res = std::max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char const* argv[]) {
+    std::string s = "pwwkew";
+
+    std::cout << Solution::lengthOfLongestSubstring(s) << std::endl;
+
+    return 0;
+}
